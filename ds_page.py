03cf579e2377323c24f636e6ef8b5d41e6395f76
slemeshevsky <sergey.lemeshevsky@gmail.com>
2022-11-07 10:47:16 +0300

    Remove fdm.

--- ds_page.py
+++ ds_page.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import streamlit as st
 import time
-from fdm.exact import T
+from exact import T
 import streamlit.components.v1 as components
 
 def form_grid_data():
